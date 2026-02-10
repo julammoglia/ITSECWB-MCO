59808f51b47c49a0be4439809ccf664263c11065
aliasCODEneo <mercadotristanneo13@gmail.com>
2026-02-10 22:03:18 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -6,7 +6,7 @@ $username = "root";
 $password = "";
 $dbname = "pluggedin_itdbadm";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli('127.0.0.1', 'root', '', 'pluggedin_itdbadm', 3307);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
